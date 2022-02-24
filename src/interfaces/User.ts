186d96994ebc9f1c095200c0158ee40c253721c0
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-02-23 21:09:50 -0300

    feat: Implementa interfaces IUser e User

---
+++ src/interfaces/User.ts
@@ -0,0 +1,14 @@
+export interface IUser {
+  username: string,
+  classe: string,
+  level: number,
+  password: string,
+}
+
+export interface User extends IUser {
+  id: number,
+  error?: {
+    code: string,
+    message: string,
+  }
+}

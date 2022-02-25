7ac49cd065520443532ea3621f9086cbd3c29bb3
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-02-24 23:53:06 -0300

    refac: Implementa interface UserLogin e extende para IUser

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,8 +1,11 @@
-export interface IUser {
+export interface UserLogin {
   username: string,
+  password: string,
+}
+
+export interface IUser extends UserLogin {
   classe: string,
   level: number,
-  password: string,
 }
 
 export interface User extends IUser {

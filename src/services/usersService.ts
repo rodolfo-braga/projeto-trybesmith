e9c7c90a9566e5350248d48243fdbf8dccb837ee
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-02-25 00:00:39 -0300

    feat(services/users): Implementa função findByEmailAndPassword

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,4 +1,4 @@
-import { IUser } from '../interfaces/User';
+import { IUser, User } from '../interfaces/User';
 import usersModel from '../models/usersModel';
 
 const create = async (user: IUser) => {
@@ -7,6 +7,18 @@ const create = async (user: IUser) => {
   return newUser;
 };
 
+const findByEmailAndPassword = async (username: string, password: string) => {
+  const user = await usersModel.findByEmailAndPassword(username, password);
+  
+  if (!user) {
+    return { 
+      error: { code: 'UNAUTHORIZED', message: 'Username or password invalid' } } as User;
+  }
+
+  return user;
+};
+
 export default {
   create,
+  findByEmailAndPassword,
 };

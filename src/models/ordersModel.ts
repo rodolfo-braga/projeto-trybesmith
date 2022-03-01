87af7b1c1a7890d292c5937640bd93dd7a2a270e
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-01 15:06:16 -0300

    feat(models/orders): Implementa função findOne

--- src/models/ordersModel.ts
+++ src/models/ordersModel.ts
@@ -1,5 +1,6 @@
-import { ResultSetHeader } from 'mysql2';
-import { NewOrder, Order } from '../interfaces/Order';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { IOrder, NewOrder, Order } from '../interfaces/Order';
+import { UserId } from '../interfaces/User';
 
 import connection from './connection';
 
@@ -22,6 +23,27 @@ const create = async (order: Order, userId: number): Promise<NewOrder> => {
   } };
 };
 
+const findOne = async (id: number): Promise<IOrder | null> => {
+  const [result] = await connection.execute<UserId[]>(
+    'SELECT userId FROM Trybesmith.Orders WHERE id = ?',
+    [id],
+  );
+   
+  if (!result.length) return null;
+
+  const { userId } = result[0];
+
+  const [productsIds] = await connection.execute<RowDataPacket[]>(
+    'SELECT id FROM Trybesmith.Products WHERE orderId = ?',
+    [id],
+  );
+
+  const products: number[] = productsIds.map((product) => product.id);
+
+  return { id, userId, products } as IOrder;
+};
+
 export default {
   create,
+  findOne,
 };
